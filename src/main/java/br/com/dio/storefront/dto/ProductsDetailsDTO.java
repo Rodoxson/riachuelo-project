b720515876d3d702e7facb5818a848a9d106a974
Rodolfo Almeida <rodolforock12@gmail.com>
2025-09-03 13:20:14 -0400

    Configurações do serviço Storefront

---
+++ src/main/java/br/com/dio/storefront/dto/ProductsDetailsDTO.java
@@ -0,0 +1,21 @@
+package br.com.dio.storefront.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductsDetailsDTO(
+
+    @JsonProperty ("id")
+    UUID id,
+    @JsonProperty ("name")
+    String name,
+    @JsonProperty("price")
+    BigDecimal price){
+
+
+}
+
+
+
---
+++ src/main/java/br/com/dio/storefront/service/IProductService.java
@@ -0,0 +1,16 @@
+package br.com.dio.storefront.service;
+
+import br.com.dio.storefront.dto.ProductInfoDTO;
+import br.com.dio.storefront.entity.ProductEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IProductService {
+
+    ProductEntity save(final ProductEntity productEntity);
+    void changeActivated(final UUID id, final boolean activate);
+    List<ProductEntity> findAllActive();
+    ProductInfoDTO findInfo(final UUID id);
+    void purchase(final UUID id);
+}
